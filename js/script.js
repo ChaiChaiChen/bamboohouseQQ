c09aa7a940b41de55ebd0e7f14779f60854115d7
Jiafu <jiafu20001003@gmail.com>
2023-04-22 22:22:11 +0800

    eighteen commit

--- js/script.js
+++ js/script.js
@@ -5,19 +5,23 @@ menu.onclick = () => {
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
 }
-var swiper = new Swiper(".home-slider",{
+var swiper = new Swiper(".home-slider", {
     spaceBetween: 30,
     centeredSlides: true,
-    autoplay:{
-        delay:7500,
-        disableOnInteraction:false,
+    autoplay: {
+        delay: 7000,
+        disableOnInteraction: false,
     },
-    pagination:{
-        el:".swiper-pagination",
-        clickable:false,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
     },
-    loop:true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
 });
+
 
 let section = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header .navbar a');
